730f4e8cb02a28974831f6ad6d7862727c69c8d5
benjuwan_5931 <benjuwan.kokusaig@gmail.com>
2024-12-24 12:05:49 +0900

    fix:アバウトページの内容及びスタイルの修正、各種ページ関連のコンポーネントのリファクタリング.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 
 export const metadata: Metadata = {
   title: "Reserve-Sys",
@@ -21,9 +23,11 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body>
+        <Header />
         <div className="globalWrapper">
           {children}
         </div>
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
 import Rooms from "./components/rooms/Rooms";
 import Calendar from "./components/schedule/calendar/Calendar";
 
 export default async function Home() {
   return (
     <main>
-      <Link href={'/about'}>使い方について</Link>
       <Rooms />
       <Calendar />
     </main>
