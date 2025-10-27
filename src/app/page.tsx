5b90668ad953651c816ad6d3a2af26a87a578c55
benjuwan <benjuwan.banquet12@gmail.com>
2025-10-28 08:16:09 +0900

    fix: 418ハイドレーションエラーの対応完了及びコード内コメントの修正.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import Rooms from "../components/rooms/Rooms";
 import Calendar from "../components/schedule/calendar/Calendar";
 
 export default async function Home() {
-  /* 418 hydration-error 対策：サーバーコンポーネントでデータ取得して子に渡す  */
+  /* 418 hydration-error 対策：
+    - 【原因】：デプロイ先（Vercel）のタイムゾーンが日本時間でないことによる時差発生
+    - 【対応】：サーバーコンポーネントで日付データを取得して子に渡す 
+  */
   // デプロイ先のタイムゾーン設定がUTCの場合に備えてJSTの日付（今日・本日）を取得
-  const jst = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' }));
-  const theToday = jst.getDate();
+  const jst: Date = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' }));
+  const theToday: number = jst.getDate();
 
   return (
     <main>
