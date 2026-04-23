41628bcbf0e47b15f98f084fc3f407907c1da4ad
benjuwan <benjuwan.banquet12@gmail.com>
2026-04-23 12:53:49 +0900

    fix: 各種ライブラリのアップデート及びREADME更新、Lintアップデートに伴うLintエラー対策リファクタリング.

--- src/components/schedule/todoItems/utils/TodoFormItemRoom.tsx
+++ src/components/schedule/todoItems/utils/TodoFormItemRoom.tsx
@@ -1,20 +1,25 @@
-import { SyntheticEvent, Dispatch, Ref, memo, SetStateAction, RefObject } from "react";
+import { SyntheticEvent, Dispatch, Ref, memo, SetStateAction, useEffect } from "react";
 import { todoItemType } from "../ts/todoItemType";
 import { roomsType } from "@/components/rooms/ts/roomsType";
 import { useHandleFormEntries } from "@/hooks/useHandleFormEntries";
 import { useCheckTimeValidation } from "../hooks/useCheckTimeValidation";
 
-function TodoFormItemRoom({ rooms, todoItems, setTodoItems, roomRef, validationTxtRef }: {
+function TodoFormItemRoom({ rooms, todoItems, setTodoItems, roomRef, setValidationTxt, validationTxt }: {
     rooms: roomsType,
     todoItems: todoItemType,
     setTodoItems: Dispatch<SetStateAction<todoItemType>>,
     roomRef: Ref<HTMLSelectElement> | undefined,
-    validationTxtRef?: RefObject<string>
+    setValidationTxt: (txt: string) => void,
+    validationTxt: string
 }) {
     const { handleFormEntries } = useHandleFormEntries();
-
     const { checkTimeValidation } = useCheckTimeValidation();
-    checkTimeValidation(todoItems, validationTxtRef);
+
+    useEffect(() => {
+        // Effect 内で State 更新関数を内包した関数を実行しているがLintエラーは発生しない
+        // 理由： Lint はコードの文脈から「このStateは不要では？」と判断した上で警告を出すため ＝ 今回の実装内容およびロジックの場合は『確かにState更新関数がEffect内で必要だな』と判断されてスルー（許可）されている
+        checkTimeValidation(todoItems, setValidationTxt, validationTxt);
+    }, [todoItems, setValidationTxt, validationTxt, checkTimeValidation]);
 
     return (
         <>
